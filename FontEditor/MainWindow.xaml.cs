ce68364952573b628a043bcb90740efbfbd6dab3
Tom Meinhold <tom.meinhold2002@gmail.com>
2021-12-23 10:19:59 +0100

    Project bug fix (Visual Studio not updating automatic)

--- FontEditor/MainWindow.xaml.cs
+++ FontEditor/MainWindow.xaml.cs
@@ -1,32 +1,28 @@
 ﻿namespace FontEditor
 {
-    using FontEditor.MVVM.ViewModel;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
     using System.Windows;
+    using System.Windows.Controls;
+    using System.Windows.Data;
+    using System.Windows.Documents;
+    using System.Windows.Input;
+    using System.Windows.Media;
+    using System.Windows.Media.Imaging;
+    using System.Windows.Navigation;
+    using System.Windows.Shapes;
 
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window
     {
-        private readonly BaseDataContext context = new();
-
         public MainWindow()
         {
-            DataContext = context;
             InitializeComponent();
-        }
-
-        private void Window_Drop(object sender, DragEventArgs e)
-        {
-            if (e.Data.GetDataPresent(DataFormats.FileDrop))
-            {
-                // Note that you can have more than one file.
-                string[] files = (string[])e.Data.GetData(DataFormats.FileDrop);
-
-                // Assuming you have one file that you care about, pass it off to whatever
-                // handling code you have defined.
-                context.CurrentTexture = new System.Uri(files[0]);
-            }
         }
     }
 }
